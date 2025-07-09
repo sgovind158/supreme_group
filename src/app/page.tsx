9c81191d0d2add4df6f736f835a167fdfae73f84
GovindSahu123 <govind.sahu@valuecreatives.com>
2025-07-09 16:22:11 +0530

    optimize com

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-import HeroSection from "./(Home)/HeroSection";
-import Vehicle360 from "./(Home)/Vehicle360";
-import GetInTouch from "./(Home)/GetInTouch";
+"use client";
+// import HeroSection from "./(Home)/HeroSection";
+// import Vehicle360 from "./(Home)/Vehicle360";
+// import GetInTouch from "./(Home)/GetInTouch";
+import dynamic from "next/dynamic";
+
+const HeroSection = dynamic(() => import("./(Home)/HeroSection"), { ssr: false });
+const Vehicle360 = dynamic(() => import("./(Home)/Vehicle360"), { ssr: false });
+const GetInTouch = dynamic(() => import("./(Home)/GetInTouch"), { ssr: false });
 
 export default function Home() {
   
